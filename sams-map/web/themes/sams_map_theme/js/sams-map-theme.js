f6fa5687fa73aff81d42d1a69007d7680b9a803f
sjibs <74805504+sjibs@users.noreply.github.com>
2024-01-10 23:45:58 +0000

    Initial commit

---
+++ sams-map/web/themes/sams_map_theme/js/sams-map-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * sams-map-theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.samsMapTheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
